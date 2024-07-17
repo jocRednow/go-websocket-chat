cdd2a3ba67d9c1401c1364b27fed97efca860361
stefankrasnow <jocrednow@gmail.com>
2024-07-17 18:08:59 +0200

    webSocket simple chat

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "golang.org/x/net/websocket"
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
 
 type Server struct {
 	conns map[*websocket.Conn]bool
@@ -12,8 +18,34 @@ func NewServer() *Server {
 	}
 }
 
-func (s *Server) handleWS()
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new incoming connection from client", ws.RemoteAddr())
+
+	s.conns[ws] = true
+
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg := buf[:n]
+		fmt.Println(string(msg))
+
+		ws.Write([]byte("thank you for the message"))
+	}
+}
 
 func main() {
-
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.ListenAndServe(":3000", nil)
 }
